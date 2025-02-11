31950e71749af7a31e41e61f433ee498e9a6c89d
waldyctt <clocktoktok@gmail.com>
2025-02-11 17:03:26 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate embedded_hal;
+extern crate rp2040_hal;
+
+//This will be the panic for error
+use panic_halt as _;
+
+//Define the RP2040 hal, which will be the main thing to control the raspberry pi pico micro-controller
+use rp2040_hal as hal;
+
+use hal::pac;
+
+//Import external library here
+use embedded_hal::digital::{OutputPin};
+use rp2040_hal::clocks::Clock;
+
+//External high-speed crystal on the Raspberry Pi Pico board = 12MHz
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+#[rp2040_hal::entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Configure GPIO25 as an output
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
